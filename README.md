56466b69315916549526161e0ce6f55d1ab3e83d
Niels Vandekeybus <progster@gmail.com>
2019-01-10 15:13:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Marawa
+npm module extracted from https://github.com/lblod/notulen-importer-service/commit/87b3014941fe33edc5d8d2b94f0429f0868c2495
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ...require('./node-walker'),
+  ...require('./rdfa-context-scanner'),
+  ...require('./rich-node'),
+  ...require('./ember-object-mock.js')
+};
