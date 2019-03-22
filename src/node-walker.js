d434df0746189cbffd0ef3c2a38917dd5173db52
Aad Versteden <madnificent@gmail.com>
2019-03-22 11:48:32 +0100

    Select Node constants described by MDN
    There's a known list, we're picking the elements from MDN.

--- src/node-walker.js
+++ src/node-walker.js
@@ -2,24 +2,17 @@ import RichNode from './rich-node';
 import {get, set} from './ember-object-mock';
 
 if( ! Node ) {
-  var Node = { ATTRIBUTE_NODE: 2,
-               CDATA_SECTION_NODE: 4,
-               COMMENT_NODE: 8,
-               DOCUMENT_FRAGMENT_NODE: 11,
-               DOCUMENT_NODE: 9,
-               DOCUMENT_POSITION_CONTAINED_BY: 16,
-               DOCUMENT_POSITION_CONTAINS: 8,
-               DOCUMENT_POSITION_DISCONNECTED: 1,
-               DOCUMENT_POSITION_FOLLOWING: 4,
-               DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC: 32,
-               DOCUMENT_POSITION_PRECEDING: 2,
-               DOCUMENT_TYPE_NODE: 10,
-               ELEMENT_NODE: 1,
-               ENTITY_NODE: 6,
-               ENTITY_REFERENCE_NODE: 5,
-               NOTATION_NODE: 12,
-               PROCESSING_INSTRUCTION_NODE: 7,
-               TEXT_NODE: 3 };
+  var Node = {
+    // Node types consumed from https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+    ELEMENT_NODE: 1, // 	An Element node like <p> or <div>.
+    TEXT_NODE: 3, // 	The actual Text inside an Element or Attr.
+    CDATA_SECTION_NODE: 4, // 	A CDATASection, such as <!CDATA[[ … ]]>.
+    PROCESSING_INSTRUCTION_NODE: 7, // 	A ProcessingInstruction of an XML document, such as <?xml-stylesheet … ?>.
+    COMMENT_NODE: 8, // 	A Comment node, such as <!-- … -->.
+    DOCUMENT_NODE: 9, // 	A Document node.
+    DOCUMENT_TYPE_NODE: 10, // 	A DocumentType node, such as <!DOCTYPE html>.
+    DOCUMENT_FRAGMENT_NODE: 11, // 	A DocumentFragment node.
+  };
 }
 
 /**
